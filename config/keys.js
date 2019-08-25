e8b3ba3f3a4752f2f1c0a264ee346bd78af549e3
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-08-26 03:24:03 +0530

    Connect to the database and add user model

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-   mongoURI: process.env.mongoURI,
-   googleClientID: process.env.googleClientID,
-   googleClientSecret: process.env.googleClientSecret
-}
