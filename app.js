4cc9799b778b597ffdb4f28ed65e51e03c519fce
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-08-26 04:40:45 +0530

    Add session middleware, verify and logout routes

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('./db/mongoose')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const passport = require('passport')
 const authRoute = require('./routes/authRoute')
 
 const app = express()
 
-app.use(authRoute)
-
 // Passport config
 require('./config/passport')(passport)
+
+app.use(cookieParser())
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// Passport middlewares
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null
+  next()
+})
+
+// Load routes
+app.use(authRoute)
 
 app.get('/', (req, res) => {
   res.send('It works')
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -10,6 +10,18 @@ router.get('/auth/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
     res.redirect('/dashboard');
-  });
+  })
+
+router.get('/auth/verify', (req, res) => {
+  if(req.user){
+    console.log(req.user)
+  }
+  else console.log('Not Auth')
+})
+
+router.get('/auth/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
 
 module.exports = router
