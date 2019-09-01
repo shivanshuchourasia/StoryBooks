874851727f0dfeb225c36166a56dacc13c9e5bbb
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-09-01 23:52:40 +0530

    Add dashboard page

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const router = new express.Router
+const Story = require('../models/story')
 const {ensureAuthenticated, ensureGuest} = require('../helpers/auth')
 
 router.get('/', ensureGuest, (req, res) => {
   res.render('index/welcome')
 })
 
-router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  res.render('index/dashboard')
+router.get('/dashboard', ensureAuthenticated, async (req, res) => {
+  try {
+    const stories = await Story.find({ owner: req.user.id })
+    res.render('index/dashboard', { stories })
+  } catch(e) {
+    res.status(404).send()
+  }
 })
 
 router.get('/about', (req, res) => {
