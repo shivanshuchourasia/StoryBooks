df617a4f0a111ae9aaaaadfec3b745f6fa55f67b
Shivanshu Chourasia <shivansh4011@gmail.com>
2019-09-08 06:56:36 +0530

    Add branch editor

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-// var env = process.env.NODE_ENV || 'development'
-// if(env === 'development'){
-//   var db = 'mongodb://127.0.0.1:27017/storybooks-dev'
-// } else {
-//   var db = process.env.mongoURI
-// }
+var env = process.env.NODE_ENV || 'development'
+if(env === 'development'){
+  var db = 'mongodb://127.0.0.1:27017/storybooks-dev'
+} else {
+  var db = process.env.mongoURI
+}
 
-var db = process.env.mongoURI
+// var db = process.env.mongoURI
 
 mongoose.connect(db, {
   useNewUrlParser: true,
